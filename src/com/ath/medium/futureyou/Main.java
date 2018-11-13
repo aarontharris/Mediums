c3d911210e406fc6fd704432d84b036d90cf2712
Aaron <aarontharris@gmail.com>
2018-11-13 15:17:44 -0800

    .

--- src/com/ath/medium/futureyou/Main.java
+++ src/com/ath/medium/futureyou/Main.java
@@ -2,9 +2,14 @@ package com.ath.medium.futureyou;
 
 public class Main {
 
-
     public static void demonstrate() {
+        ShrinkRay ray1 = new ShrinkRay();
+        // ray1 is confusing -- perhaps I'll look what others did and copy/paste their code?
 
+        ShrinkRayAdvanced ray2 = new ShrinkRayAdvanced();
+        if (!ray2.isCharged())
+            ray2.recharge();
+        ray2.fire();
     }
 
 }
